0ccfd7c8d5ffacc7bd94eb24456e54e0a61025f5
Anikesh Dhanokar <anikesh13.linuxbean@gmail.com>
2019-03-26 14:46:39 +0530

    gfdsg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService, FacebookLoginProvider, GoogleLoginProvider, LinkedInLoginProvider, SocialUser } from 'angularx-social-login';
 import isIncognito from 'is-incognito';
 import { LogService } from './log.service';
 import { isPlatformBrowser } from '@angular/common';
-import { BarcodeFormat } from '@zxing/library';
+import { BarcodeFormat, Result } from '@zxing/library';
+import { ZXingScannerComponent } from './modules/zxing-scanner/zxing-scanner.component';
+import { VERSION } from '@angular/forms';
 // const isIncognito = require('is-incognito');
 
 
@@ -26,7 +28,71 @@ export class AppComponent implements OnInit {
     @Inject(PLATFORM_ID) private platformId: Object) { }
 
 
+    ngVersion = VERSION.full;
+
+  @ViewChild('scanner')
+  scanner: ZXingScannerComponent;
+
+  hasDevices: boolean;
+  hasPermission: boolean;
+  qrResultString: string;
+  qrResult: Result;
+
+  availableDevices: MediaDeviceInfo[];
+  currentDevice: MediaDeviceInfo;
+
+
+  displayCameras(cameras: MediaDeviceInfo[]) {
+    console.debug('Devices: ', cameras);
+    this.availableDevices = cameras;
+  }
+
+  handleQrCodeResult(resultString: string) {
+    console.debug('Result: ', resultString);
+    this.qrResultString = resultString;
+  }
+
+  onDeviceSelectChange(selectedValue: string) {
+    console.debug('Selection changed: ', selectedValue);
+    this.currentDevice = this.scanner.getDeviceById(selectedValue);
+  }
+
+  stateToEmoji(state: boolean): string {
+
+    const states = {
+      // not checked
+      undefined: '❔',
+      // failed to check
+      null: '⭕',
+      // success
+      true: '✔',
+      // can't touch that
+      false: '❌'
+    };
+
+    return states['' + state];
+  }
+
+
   ngOnInit() {
+
+    this.scanner.camerasFound.subscribe((devices: MediaDeviceInfo[]) => {
+      this.hasDevices = true;
+      this.availableDevices = devices;
+
+      // selects the devices's back camera by default
+      // for (const device of devices) {
+      //     if (/back|rear|environment/gi.test(device.label)) {
+      //         this.scanner.changeDevice(device);
+      //         this.currentDevice = device;
+      //         break;
+      //     }
+      // }
+    });
+
+    this.scanner.camerasNotFound.subscribe(() => this.hasDevices = false);
+    this.scanner.scanComplete.subscribe((result: Result) => this.qrResult = result);
+    this.scanner.permissionResponse.subscribe((perm: boolean) => this.hasPermission = perm);
 
 
     this.log.getDetails().subscribe(hello => {
